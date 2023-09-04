9d91cc9dcc6c4aff71f074f95ad8a5930bff99fe
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-04 15:23:44 +0300

    Update Redis.py

--- mody/Redis.py
+++ mody/Redis.py
@@ -1,9 +1,9 @@
 from redis import StrictRedis
 
 # تعيين المعلمات الخاصة بالاتصال
-host = "containers-us-west-65.railway.app"  # عنوان خادم Redis
-port = 8035  # المنفذ الذي ترغب في استخدامه
-password = "h8ZzUCIJ26cFCneVwdkO"  # كلمة المرور إذا كانت مطلوبة
+host = "containers-us-west-177.railway.app"  # عنوان خادم Redis
+port = 7718  # المنفذ الذي ترغب في استخدامه
+password = "E4Zm1vpbzc0R3CXM31Hh"  # كلمة المرور إذا كانت مطلوبة
 
 # إنشاء اتصال Redis
 db = StrictRedis(host=host, port=port, password=password, decode_responses=True)
